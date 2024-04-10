4a1a2a78a907fe16966d6887da7492cad709197c
d1msk1y <exect81@gmail.com>
2024-04-10 16:29:34 +0200

    Switch to most recent python lib, use SGrDevice instead

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from sgr.sgr_library.generic_interface import GenericInterface
-import asyncio
-from fastapi import FastAPI, File, UploadFile, HTTPException, status
-from pydantic import BaseModel, Field
-import uvicorn
 import os
 from typing import Optional, List, Dict, Any
+
+import uvicorn
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from pydantic import BaseModel, Field
+
+from sgr.sgr_library.sgr_device import SGrDevice
 
 app = FastAPI(
     title="SmartGridready Intermediary API",
@@ -12,22 +13,25 @@ app = FastAPI(
     version="1.0.0",
 )
 
-instance_counter = 1 
-interfaces = {} 
+instance_counter = 1
+interfaces = {}
+
 
 class InitializationResponse(BaseModel):
     status: str
     instance_id: Optional[str] = None
 
-@app.post("/instances", response_model=InitializationResponse, tags=["instances"], summary="Initialize a new instance of the Generic Interface.")
+
+@app.post("/instances", response_model=InitializationResponse, tags=["instances"],
+          summary="Initialize a new instance of the Generic Interface.")
 async def initialize(xml: UploadFile = File(...), ini: UploadFile = File(...)):
     global instance_counter, interfaces
 
     ini_file_ext = os.path.splitext(ini.filename)[-1].lower()
     xml_file_ext = os.path.splitext(xml.filename)[-1].lower()
 
-    print( ini_file_ext)
-    
+    print(ini_file_ext)
+
     if ini_file_ext not in ['.ini'] or xml_file_ext not in ['.xml']:
         raise HTTPException(status_code=400, detail="Incorrect file types uploaded. Expecting .ini and .xml.")
 
@@ -39,14 +43,14 @@ async def initialize(xml: UploadFile = File(...), ini: UploadFile = File(...)):
     # Async file saving
     with open(ini_path, "wb") as buffer:
         buffer.write(await ini.read())
-    
+
     with open(xml_path, "wb") as buffer:
         buffer.write(await xml.read())
 
     if f"{instance_id}" not in interfaces:
         interfaces[f"{instance_id}"] = {}
-    
-    interfaces[f"{instance_id}"]["generic_interface"] = GenericInterface(xml_path, ini_path)
+
+    interfaces[f"{instance_id}"]["generic_interface"] = SGrDevice()
     interfaces[f"{instance_id}"]["xml"] = xml.filename
     interfaces[f"{instance_id}"]["ini"] = ini.filename
 
@@ -54,7 +58,8 @@ async def initialize(xml: UploadFile = File(...), ini: UploadFile = File(...)):
     os.remove(ini_path)
     os.remove(xml_path)
 
-    await interfaces[f"{instance_id}"]["generic_interface"].authenticate()
+    interfaces[f"{instance_id}"]["generic_interface"].update_xml_spec(xml_path).update_config(ini_path).connect()
+    await interfaces[f"{instance_id}"]["generic_interface"].connect()
 
     instance_counter += 1
     return InitializationResponse(status="success", instance_id=instance_id)
@@ -63,25 +68,28 @@ async def initialize(xml: UploadFile = File(...), ini: UploadFile = File(...)):
 # get initialized instances
 @app.get("/instances", response_model=Dict[str, Any], tags=["instances"], summary="get initialized instances")
 async def get_instances():
-  # return only the instance ids, xml and ini paths
-  result = {
-      key: {
-          'instance_id': key,
-          'xml': value['xml'],
-          'ini': value['ini']
-      }
-      for key, value in interfaces.items()
-  }
-  return result
+    # return only the instance ids, xml and ini paths
+    result = {
+        key: {
+            'instance_id': key,
+            'xml': value['xml'],
+            'ini': value['ini']
+        }
+        for key, value in interfaces.items()
+    }
+    return result
 
 
 Datapoint = Dict[str, Dict[str, List[str]]]
+
 
 class ResponseData(BaseModel):
     status: str = Field(..., example="success")
     data: Dict[str, Dict[str, Dict[str, float]]]
 
-@app.post("/get", response_model=ResponseData, tags=["Generic Interface"], summary="Get values from the Generic Interface.")
+
+@app.post("/get", response_model=ResponseData, tags=["Generic Interface"],
+          summary="Get values from the Generic Interface.")
 async def get(data: Datapoint):
     """
     ## Retrieve datapoint of specific functional profile 
@@ -145,10 +153,10 @@ async def get(data: Datapoint):
             for dpname in dpnames:
                 # Your logic to retrieve the value for fpname and dpname
                 try:
-                  val = await interfaces[instance_id]["generic_interface"].getval(fpname, dpname)
-                  result_dict[instance_id][fpname][dpname] = val
+                    val = await interfaces[instance_id]["generic_interface"].getval(fpname, dpname)
+                    result_dict[instance_id][fpname][dpname] = val
                 except Exception as e:
-                  err_string += f"Error getting value for {fpname}.{dpname}: {e}\n"
+                    err_string += f"Error getting value for {fpname}.{dpname}: {e}\n"
 
     status = err_string if err_string else 'success'
     return {'status': status, 'data': result_dict}
